45fa49be273bfe3f3a5561d8f089a5525257d470
proniket <proniket01@gmail.com>
2022-03-17 14:38:07 +0530

    added react toastify

--- src/components/Collection/CollectionFillter.tsx
+++ src/components/Collection/CollectionFillter.tsx
@@ -4,10 +4,22 @@ import {
   useMoralisQuery,
 } from "react-moralis";
 import { useDispatch } from "react-redux";
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { getFilterCategory } from "../../redux/features/dashboard/dashboardSlice";
+toast.configure();
+
+
+
+
 
 const ColletctionFillter = () => {
+  const notify = () => {
+    // toast("Hi I'm React Toastify !");
+    // inbuilt-notification
+      toast.success("Hi I'm React Toastify !")
+  };
   const dispatch = useDispatch();
   const cotegory: any = useMoralisQuery("Category");
   const { Moralis }: MoralisContextValue = useMoralis();
@@ -25,13 +37,15 @@ const ColletctionFillter = () => {
 
   return (
     <div className="collection_fillter">
-      <h2 className="collection_fillter--title">Collections</h2>
+      <h2 className="collection_fillter--title" onClick={notify}>Collections</h2>
+      {/* <ToastContainer /> */}
       <div className="collection_fillter--items_container">
         <div
           className="collection_fillter--items_container--items"
           onClick={() => dispatch(getFilterCategory(allCollection.data))}
         >
           All
+         
         </div>
         {cotegory.data.map(({ attributes }: any, i: number) => (
           <div
@@ -46,3 +60,8 @@ const ColletctionFillter = () => {
   );
 };
 export default ColletctionFillter;
+
+
+
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
